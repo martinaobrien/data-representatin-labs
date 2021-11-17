48a9289edaf2e6ee9d084adad3aa6d0e22f719b0
martinaobrien <G00376463@gmit.ie>
2021-11-17 18:04:48 +0000

    Update server.py
    Date Rep lab

--- Week_08/server.py
+++ Week_08/server.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, url_for, request, redirect, abort
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path= '', static_folder='staticpages')
 
 @app.route('/')
 def index():
-    return "Hello, World"
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
+    return "served by Login"
+
+@app.route('/user')
+def getUser():
+    return "served by get User"
+
+@app.route('/user/<int:id>')
+def findByIdUser(id):
+    return "served by findByIdUser with id = "+str(id)
+
+@app.route('/user', methods= ['POST'])
+def createUser():
+    return "served by createUser"
+
+def getUser():
+    return "served by get User"
+
+@app.route('/demo_url_for')
+def demoUrlFor():
+    returnString = "url for index is "+ url_for('index')
+    returnString += "<br/>"
+    returnString += "url for findByIdUser" + url_for('findByIdUser', id = 12)
+    return returnString
+
+@app.route("/demo_request", methods=['POST', 'GET', 'DELETE'])
+def demoRequest():
+    return request.method 
 
 if __name__ == '__main__':
     print("in if")
